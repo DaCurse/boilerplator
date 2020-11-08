0bf894dbf058275107ec711de7b0e9938e2fcce8
DaCurse <dacurse0@gmail.com>
2020-11-08 18:42:33 +0200

    Improve documentation

--- src/errors/command-wrapper.js
+++ src/errors/command-wrapper.js
@@ -1,7 +1,7 @@
 /**
  * Wraps commands in a try/catch to print any thrown errors
- * @param {Function} cmd
- * @param  {...any[]} args
+ * @param {Function} cmd Command to run
+ * @param  {...any} args Args to pass to command
  */
 module.exports = (cmd, ...args) => {
   try {
