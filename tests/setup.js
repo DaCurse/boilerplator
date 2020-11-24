dc9e74c296f29c7de29699746a682c509aa91d02
DaCurse <dacurse0@gmail.com>
2020-11-24 13:24:37 +0200

    Added tests for config command

--- tests/setup.js
+++ tests/setup.js
@@ -6,10 +6,12 @@ const { join } = require('path');
  * @param {string} cmd Command name
  * @param {string} args Command like arguments
  * @param {string} envPath Path to a testing .env file
+ * @param {object} options Options for `execSync`
  */
-global.execCommand = (cmd, args, envPath) => {
+global.execCommand = (cmd, args, envPath, options) => {
   const cmdPath = join(process.cwd(), cmd);
   return execSync(
-    `node -r dotenv/config ${cmdPath} ${args} dotenv_config_path=${envPath}`
+    `node -r dotenv/config ${cmdPath} dotenv_config_path=${envPath} ${args}`,
+    options
   );
 };
